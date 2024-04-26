549cb8b5985dfbc44b88bf83c77cc0cb68bf3f1d
Tanzir Ratul <tanzir.ratul@gmail.com>
2024-04-26 19:42:40 +0600

    map style set

--- app/src/main/java/com/example/googlemap/MainActivity.kt
+++ app/src/main/java/com/example/googlemap/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.googlemap
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
 
 class MainActivity : AppCompatActivity() {
+    val fragment = MapsFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+             insets
+         }*/
     }
+
+   /* override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.map_types_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        fragment.mapType(item)
+        return true
+    }*/
 }
--- app/src/main/java/com/example/googlemap/MapsFragment.kt
+++ app/src/main/java/com/example/googlemap/MapsFragment.kt
@@ -3,7 +3,11 @@ package com.example.googlemap
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 
@@ -12,29 +16,24 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
-    private var map : GoogleMap? = null
+    private var map: GoogleMap? = null
+
+
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
         map = googleMap
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15f))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15f))
         map?.uiSettings.apply {
-            this?.isZoomControlsEnabled  = true
-            this?.isScrollGesturesEnabled = false
+            this?.isZoomControlsEnabled = true
+            this?.isScrollGesturesEnabled = true
             this?.isZoomGesturesEnabled = true
         }
+        setMapStyle(map)
     }
 
     override fun onCreateView(
@@ -49,5 +48,82 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_types_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.normal_map -> {
+                map?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            }
+
+            R.id.hybrid_map -> {
+                map?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            }
+
+            R.id.terrain_map -> {
+                map?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            }
+
+            R.id.sat_map -> {
+                map?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            }
+
+            R.id.none_map -> {
+                map?.mapType = GoogleMap.MAP_TYPE_NONE
+            }
+
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+        return true
+    }
+
+    fun mapType(item: MenuItem) {
+        Log.d("MapFragment", "Menu item selected: ${item.itemId}") // Check if this method is called
+
+        when (item.itemId) {
+            R.id.normal_map ->
+                map?.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            R.id.hybrid_map ->
+                map?.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+            R.id.terrain_map ->
+                map?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+
+            R.id.sat_map ->
+                map?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
+            R.id.none_map ->
+                map?.mapType = GoogleMap.MAP_TYPE_NONE
+
+
+        }
+
+
+    }
+
+    private fun setMapStyle(googleMap: GoogleMap?) {
+        try {
+
+            val success = googleMap?.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(), R.raw.style
+                )
+            )
+            if (success==false) {
+                Log.d("Maps", "Style parsing failed")
+            }
+        } catch (e: Exception) {
+            Log.e("Maps", e.toString())
+        }
     }
 }
